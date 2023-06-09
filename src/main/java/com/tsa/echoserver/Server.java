840f6931ad6de60525424f92071f8bbf5512501e
Serhii Trehubenko <tsa85.ca@gmail.com>
2023-06-09 19:06:53 +0300

    Servlet through telnet multithreaded

--- src/main/java/com/tsa/echoserver/Server.java
+++ src/main/java/com/tsa/echoserver/Server.java
@@ -1,40 +1,55 @@
 package com.tsa.echoserver;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Objects;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 public class Server {
 
     static final String STOP = "stop";
-
     static final String NEXT = "next";
-
     private static final String ECHO = "echo ";
 
     public static void start(int port) {
-        try (var server = new ServerSocket(port)) {
-            String inputMessage = "";
-            byte[] buffer = new byte[1024];
+
+        try (final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(4);
+                var server = new ServerSocket(port)) {
+
+            while (true) {
+                var socket = server.accept();
+//                Thread thread = new Thread(() -> answerRequest(socket));
+//                thread.start();
+                scheduledThreadPoolExecutor.execute(() -> answerRequest(socket));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void answerRequest(Socket socket) {
+        String inputMessage = "";
+        byte[] buffer = new byte[150];
+
+        try (Socket incomeSocket = socket;
+             var input = socket.getInputStream();
+             var output = socket.getOutputStream()) {
 
             while (!Objects.equals(STOP, inputMessage)) {
-                inputMessage = "";
-                try (var socket = server.accept();
-                     var input = socket.getInputStream();
-                     var output = socket.getOutputStream()) {
+                int count = 0;
+                byte incomeChar = 0;
 
-                    while (!Objects.equals(NEXT, inputMessage) & !Objects.equals(STOP, inputMessage)) {
-                        int count = input.read(buffer);
-                        inputMessage = new String(buffer, 0, count);
-                        String response = ECHO + inputMessage;
-                        output.write(response.getBytes());
+                while (incomeChar != 13) {
+                    incomeChar = (byte) input.read();
+                    if (incomeChar != 10 && incomeChar != 13) {
+                        buffer[count] = incomeChar;
+                        count++;
                     }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
-
+                inputMessage = new String(buffer, 0, count);
+                String response = ECHO + inputMessage + "\n\r";
+                output.write(response.getBytes());
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
